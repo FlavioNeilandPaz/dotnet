622471ab0d90df35ce3fdb2e9287f69abe74b5d3
FlavioNeilandPaz <flavioneilandpaz@gmail.com>
2021-10-05 12:27:43 -0300

    commint on main

---
+++ Review/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno:");
+            Console.WriteLine("2 - Listar alunos:");
+            Console.WriteLine("3 - Calcular média geral:");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar pessoas
+                        break;
+                    case "2":
+                        //TODO: listar pessoas
+                        break;
+                    case "3":
+                        //TODO: calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();               
+
+                }
+
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1 - Inserir novo aluno:");
+                Console.WriteLine("2 - Listar alunos:");
+                Console.WriteLine("3 - Calcular média geral:");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+
+            }
+        }
+    }
+}
